744aad7cabd661c4e40dce27c7331486315602eb
Oliver <oliver.grant@gmail.com>
2019-01-13 00:50:33 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  name=value
 end
 
-def argue
-  
+def argue (a)
+  return a
 end
 
-def greeting
+def greeting (name,greeting)
 
 end
 
 def return_a_value
-
+  a="Nice"
+  return a
 end
 
 def last_evaluated_value
-
+  a="expert"
 end
 
-def pizza_party
-
+def pizza_party(top="cheese")
+  return top
 end

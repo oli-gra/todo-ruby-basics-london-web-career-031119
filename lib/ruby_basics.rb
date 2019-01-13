d886f3418221c36da116c9efd69ac58b6ea8fb25
Oliver <oliver.grant@gmail.com>
2019-01-13 00:43:40 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,25 +3,25 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  
+  name=value
 end
 
-def argue
-  
+def argue (a)
+  return a
 end
 
-def greeting
+def greeting (greeting="Hello",name="You")
 
 end
 
 def return_a_value
-
+  a="Nice"
+  return a
 end
 
 def last_evaluated_value
-
+  a="expert"
 end
 
 def pizza_party
-
 end
